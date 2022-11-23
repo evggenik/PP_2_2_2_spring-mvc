7e44aef985686b1ddb6a580cbd0e24cdcc0a930f
Evgeny Buchelnikov <gru554@yandex.ru>
2022-11-23 22:59:01 +1000

    Finis coronat opus

---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,26 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarService {
+    private List<Car> cars;
+
+//    public CarService(List<Car> cars)
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car(1, "car1", 1));
+        cars.add(new Car(2, "car2", 2));
+        cars.add(new Car(3, "car3", 3));
+        cars.add(new Car(4, "car4", 4));
+        cars.add(new Car(5, "car5", 5));
+    }
+
+    public List<Car> countCars(int count) {
+        return count >= 5 ? cars : cars.subList(0, count);
+    }
+}
